385b6f18570c267dec97fbe6a5146a343611ebca
StijnDefour <defourstijn10@gmail.com>
2018-01-30 14:34:44 +0100

    server aangepast

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Listening on port 3000');
-});
+var db
+
+MongoClient.connect('mongodb://localhost:27017/posts', (err, database) => { //products is database + lijn 37
+  if (err) return console.log(err)
+  db = database // from version 3 : db = database.db('products')
+  app.listen(process.env.PORT || 4000, () => {
+    console.log('Listening on port 4000')
+  })
+})
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,7 +24,12 @@ app.get('/', (req, res) => {
 });
 
 // List all posts
-app.get('/posts', (req, res) => {});
+app.get('/posts', (req, res) => {
+  db.collection('posts').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('posts.ejs', {posts: result})
+  })
+});
 
 // Show the search form
 app.get('/search', (req, res) => {
@@ -25,4 +37,11 @@ app.get('/search', (req, res) => {
 });
 
 // Find all comments for post
-app.post('/search', (req, res) => {});
+app.post('/search', (req, res) => {
+  var query = { name: req.body.title }
+  db.collection('posts').find(query).toArray(function(err, result) {
+    if (err) return console.log(err)
+    if (result != '')
+        res.render('search_result.ejs', { post: result[0] })
+  });
+});
